84dba87cc9f44043aad5aee77712671dbbf2621e
ABDULLAH ALI <abdullahalicoco123@gmail.com>
2026-07-05 20:32:50 +0500

    Initial commit - Weather App

---
+++ first.js
@@ -0,0 +1,32 @@
+const cityInput = document.getElementById("search-box");
+const searchButton = document.getElementById("search-btn");
+const cityNameElement = document.getElementById("city-name");
+const temperatureElement = document.getElementById("temperature");
+const humidityElement = document.getElementById("humidity");
+const windSpeedElement = document.getElementById("wind-speed");
+
+searchButton.addEventListener("click",fetchWeatherData);
+async function fetchWeatherData() {
+    const cityName = cityInput.value;
+    const apiKey = "855cf29df13fcb4afe4b05974c3e65ca";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+try {
+    const response = await fetch(url)
+     const data = await response.json()
+    cityNameElement.innerText = data.name;
+    temperatureElement.innerText = `${data.main.temp}°C`
+    humidityElement.innerText = `Humidity: ${data.main.humidity}%`;
+
+windSpeedElement.innerText = `Wind: ${data.wind.speed} m/s`;
+    if(!response.ok) {
+        throw new Error("City not Found!")
+    }
+    
+} catch(error){
+    console.log(error)
+    alert("City not found.Please enter a valid city name.");
+    
+}
+ 
+}
